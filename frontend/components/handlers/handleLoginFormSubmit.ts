0c14cd62864dbaeb9d0c156847313870de47c132
Liam Viray <neshiviray@gmail.com>
2025-03-18 19:06:26 +0800

    Updated Credentials Logic
    catches invalid credentials error

--- frontend/components/handlers/handleLoginFormSubmit.ts
+++ frontend/components/handlers/handleLoginFormSubmit.ts
@@ -3,40 +3,44 @@ import { LoginFormType } from "../LoginForm";
 import { z } from "zod";
 import { loginSchema } from "@/lib/validations";
 import { signIn } from "next-auth/react";
-import { Session } from "next-auth";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 type handleLoginFormSubmitType = {
   e: React.FormEvent<HTMLFormElement>;
+  loginForm: LoginFormType;
   setIsPending: React.Dispatch<React.SetStateAction<boolean>>;
   setError: React.Dispatch<
     React.SetStateAction<Record<string, string> | undefined>
   >;
-  loginForm: LoginFormType;
-  rememberMe: boolean;
 };
 
 export const handleLoginFormSubmit = async ({
   e,
+  loginForm,
   setIsPending,
   setError,
-  loginForm,
-  rememberMe,
 }: handleLoginFormSubmitType) => {
   e.preventDefault();
   setIsPending(true);
 
   try {
-    if (rememberMe) {
-      localStorage.setItem("rememberMeEmail", loginForm.email);
-    }
-
     const formValues = {
       email: loginForm.email,
       password: loginForm.password,
     };
 
     await loginSchema.parseAsync(formValues);
-    await signIn("credentials", loginForm, { redirect: false });
+    const result = await signIn("credentials", {
+      ...formValues,
+      redirect: false,
+    });
+
+    if (!result?.error) {
+      window.location.href = "/home";
+    } else {
+      setError({ credentials: "Invalid Credentials. Try Again." });
+      setIsPending(false);
+    }
   } catch (error) {
     if (error instanceof z.ZodError) {
       const fieldErrors = error.flatten().fieldErrors;
