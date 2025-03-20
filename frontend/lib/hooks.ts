0cbb9ac078b97e1fa6a566b2cfb2d62c6c9dab7a
Liam Viray <neshiviray@gmail.com>
2025-03-20 20:31:33 +0800

    Updated useRememberMe hook

--- frontend/lib/hooks.ts
+++ frontend/lib/hooks.ts
@@ -3,20 +3,20 @@ import Cookies from "universal-cookie";
 
 export const useRememberMe = () => {
   const cookies = new Cookies();
+  const key = "rememberMeEmail";
 
   const [updateEmail, setUpdateEmail] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
-  const key = "rememberMeEmail";
 
   useEffect(() => {
     if (!!cookies.get(key)) {
+      setUpdateEmail(cookies.get(key));
       setRememberMe(true);
     }
   }, []);
 
   useEffect(() => {
     if (rememberMe) {
-      console.log("updateEmail", updateEmail);
       cookies.set(key, updateEmail);
     } else {
       cookies.remove(key);
