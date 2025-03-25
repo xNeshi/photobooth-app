e3435819f8265daca528d0bfd89f7ad1dec54659
Yuuugoo <mdtcristoles2021@plm.edu.ph>
2025-03-25 18:30:20 +0800

    Removed auth after registration,  auth user after oauth

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,8 +43,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
         return UserResource::make(Auth::user());
     }
 
